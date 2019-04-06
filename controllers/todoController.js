dde2e024a4e4327300fc62c2e0f865f6b940cc43
spol9967 <spol421@gmail.com>
2019-04-06 13:30:44 +0530

    mongo api url added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,7 @@
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
-var url = 'mongodb://127.0.0.1:27017'; //url of DB
+var url = 'mongodb+srv://root:root@ama-ylzfp.mongodb.net/test?retryWrites=true'; //url of DB
 var urlencodedParser = bodyParser.urlencoded({
     extented: false
 });
